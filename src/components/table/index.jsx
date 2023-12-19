a3ffa9a49edf4cffcd591c1e50eb8c9bd580f1d6
yangqianjuan <1072483500@qq.com>
2023-12-19 19:58:43 +0800

    table 内滚动

--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -1,59 +1,170 @@
 // 自己简单封装的表格组件，taro 和微信都没有table组件
-import { Component } from "react";
-import { View, Text, ScrollView } from "@tarojs/components";
+import { Component, useEffect, useState } from "react";
+import { View, Text, ScrollView, StickyHeader, Icon } from "@tarojs/components";
 import "./index.scss";
 
 //原理 循环生成表头，循环数据根据表头填入每一列
-export default class AtTable extends Component {
-    constructor(){
-        super(...arguments)
+
+export default function AtTable(props) {
+  const onScrollToUpper = function () {};
+  const [loading, setLoading] = useState(false);
+  const [isEnd, setEnd] = useState(false);
+  const [current, setCurrent] = useState(1);
+  const [dataSource, setDataSource] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const exampledataSource = [
+    {
+      username: "小红",
+      count: "1",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "2",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "3",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "4",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "5",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "6",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "7",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "8",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "9",
+      gb: "321",
+      dbd: "30%",
+    },
+    {
+      username: "小红",
+      count: "10",
+      gb: "321",
+      dbd: "30%",
+    },
+  ];
+  useEffect(() => {
+    setDataSource(exampledataSource);
+    setLoading(false);
+  }, []);
+  useEffect(() => {
+    setColumns(props.columns);
+    setDataSource(exampledataSource);
+    setLoading(false);
+    setEnd(false);
+    setCurrent(1);
+  }, [props.columns]);
+  useEffect(() => {
+    if (current < 10) {
+      setDataSource((data) => [...data, ...exampledataSource]);
+      console.log(dataSource);
+      setLoading(false);
+    } else {
+      console.log("没有数据啦");
+      setEnd(true);
+      setLoading(false);
     }
-  onScrollToUpper() {}
-
-  // or 使用箭头函数
-  // onScrollToUpper = () => {}
-
-  onScroll(e){
-    console.log(e.detail)
-  }
-  render() {
-    const scrollStyle = {
-        height: "150px",
-        // width: "320px",
-      };
-      const scrollTop = 0;
-      const Threshold = 20;
-    return (
-        
+  }, [current]);
+  const onScrollToLower = () => {
+    console.log("滚动到底啦-------");
+    if (isEnd || loading) return;
+    setLoading(true);
+    setCurrent((cur) => cur + 1);
+  };
+  const onScroll = function (e) {
+    console.log(e.detail);
+  };
+  const scrollStyle = {
+    height: "150px",
+    // width: "100%",
+  };
+  const scrollTop = 0;
+  const Threshold = 20;
+  return (
+    <ScrollView
+      className="scrollview"
+      scrollY
+      scrollX
+      scrollWithAnimation
+      scrollTop={scrollTop}
+      style={scrollStyle}
+      lowerThreshold={Threshold}
+      upperThreshold={Threshold}
+      onScrollToLower={onScrollToLower}
+      onScrollToUpper={onScrollToUpper}
+      onScroll={onScroll}
+    >
       <View className="table">
-        <View className='mask'>
         <View className="tr bg-header">
-          {this.props.columns.map((item, index) => {
+          {columns.map((item, index) => {
             return (
-              <View className="th" style={{ width: item.width }}>
-                {item.title}
+              <View
+                className="th"
+                style={{ width: item.width }}
+                key={item.dataIndex}
+              >
+                <view
+                  className="mask"
+                  onClick={() => {
+                    const value = item.sort === "asc" ? "des" : "asc";
+                    props.changeCloums(item.dataIndex, value);
+                  }}
+                >
+                  {item.title}
+                  {item.sort && (
+                    <View
+                      className={[
+                        "arrow",
+                        item.sort === "des" ? "des" : "asc",
+                      ].join(" ")}
+                      style={{
+                        transform: `rotate(${
+                          item.sort === "des" ? 135 : -45
+                        }deg)`,
+                      }}
+                    ></View>
+                  )}
+                </view>
               </View>
             );
           })}
         </View>
-        </View>
-       
-     
-        {/* <ScrollView
-        className="scrollview"
-        scrollY
-        scrollWithAnimation
-        scrollTop={scrollTop}
-        style={scrollStyle}
-        lowerThreshold={Threshold}
-        upperThreshold={Threshold}
-        onScrollToUpper={this.onScrollToUpper.bind(this)}
-        onScroll={this.onScroll}
-      > */}
-     {this.props.dataSource.map((item, index) => {
+
+        {dataSource.map((item, index) => {
           return (
             <View className="tr bg-line">
-              {this.props.columns.map((item2, index2) => {
+              {columns.map((item2, index2) => {
                 if (item2.render) {
                   return (
                     <View className="td" style={{ width: item2.width }}>
@@ -75,9 +186,10 @@ export default class AtTable extends Component {
             </View>
           );
         })}
-      {/* </ScrollView> */}
-        
+
+        {loading && <View className="loading">加载中....</View>}
+        {isEnd && <View className="loading">欧吼，加载完了。。。。</View>}
       </View>
-    );
-  }
+    </ScrollView>
+  );
 }
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,155 +1,52 @@
+import { useState } from "react";
 import { View, Text, ScrollView } from "@tarojs/components";
 import Taro, { useLoad } from "@tarojs/taro";
 import "./index.scss";
 import AtTable from "../../components/table";
 
 export default function Index() {
-  //   useLoad(() => {
-  //     console.log("Page loaded.");
-  //     Taro.getLocation({
-  //       type: "gcj02",
-  //       success: (res) => {
-  //         console.log(res);
-  //       },
-  //       fail: (err) => {
-  //         // 处理获取位置信息失败的情况
-  //         console.error("获取位置信息失败：", err);
-  //       },
-  //     });
-  //   });
-
-  //需要传进来的数据示例
-  const exampledataSource = [
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-    {
-      username: "小红",
-      count: "123",
-      gb: "321",
-      dbd: "30%",
-    },
-  ];
   //需要传进来的表头数据示例
   const examplecolumns = [
     {
       title: "名称",
       dataIndex: "username",
-      width:'30%'
+      width: "20%",
+      sort: "asc",
     },
     {
       title: "含量",
       dataIndex: "count",
-      width:'30%'
+      width: "20%",
+      sort: "des",
     },
     {
       title: "标准",
       dataIndex: "gb",
-      width:'30%'
+      width: "20%",
     },
     {
       title: "达标度",
       dataIndex: "dbd",
-      width:'30%'
+      width: "20%",
     },
   ];
-  const scrollStyle = {
-    height: "150px",
-    // width: "320px",
-  };
-  const scrollTop = 0;
-  const Threshold = 20;
-  const vStyleA = {
-    height: "150px",
-    backgroundColor: "rgb(26, 173, 25)",
-    width: "2000px",
-    display: "inline-block",
-  };
-  const vStyleB = {
-    height: "150px",
-    backgroundColor: "rgb(39,130,215)",
-    width: "2000px",
-    display: "inline-block",
-  };
-  const vStyleC = {
-    height: "150px",
-    backgroundColor: "rgb(241,241,241)",
-    color: "#333",
-    width: "2000px",
-    display: "inline-block",
-  };
+  const [cloumns, setCloumns] = useState(examplecolumns);
 
- 
+  const handleChangeCloums = (title, value) => {
+    const neCloums = cloumns.map((item) => {
+      console.log(title, value);
+      if (item.dataIndex === title) {
+        item.sort = value;
+      }
+      return item;
+    });
+    setCloumns(neCloums);
+  };
 
   return (
     <View className="container">
-      {/* <Text>Hello worldxxxxx</Text> */}
-      {/* <ScrollView
-        className="scrollview"
-        scrollY
-        scrollX
-        scrollWithAnimation
-        scrollTop={scrollTop}
-        style={scrollStyle}
-        lowerThreshold={Threshold}
-        upperThreshold={Threshold}
-        onScrollToUpper={onScrollToUpper}
-        onScroll={onScroll}
-      >
-        <AtTable dataSource={exampledataSource} columns={examplecolumns} />
-      </ScrollView> */}
-         <AtTable dataSource={exampledataSource} columns={examplecolumns} />
+      <view className="nav">其他布局</view>
+      <AtTable columns={cloumns} changeCloums={handleChangeCloums} />
     </View>
   );
 }
